2f8c581cf8534d5743d7c1e454dcc802c549b874
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2025-10-16 22:57:07 +0200

    Fix usage of unavailable macro

--- src/arduino_helpers.h
+++ src/arduino_helpers.h
@@ -1,7 +1,6 @@
 #ifndef ARDUINO_HELPERS_H_
 #define ARDUINO_HELPERS_H_
 
-#include "HeliOS.h"
 
 #ifdef __cplusplus
 
@@ -9,6 +8,7 @@
   defined(ARDUINO_TEENSY_MICROMOD) || defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY36) || defined(ARDUINO_TEENSY35) || \
   defined(ARDUINO_TEENSY31) || defined(ARDUINO_TEENSY32) || defined(ARDUINO_TEENSY30) || defined(ARDUINO_TEENSYLC)
   #include <Arduino.h>
+  #include "HeliOS.h"
 
     String xByte2String(xSize size_, xByte *bytes_);
 
